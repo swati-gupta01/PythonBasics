b152dd4e1e4f47b86ea2a2cb6ed53de5d33df5f4
Swati Gupta <swa.gupta01@gmail.com>
2018-09-02 13:00:31 +0530

    private variable created a new copy , if we try to midify that

--- Encapsulation/private_var.py
+++ Encapsulation/private_var.py
@@ -6,11 +6,12 @@ called as ENCAPSULATION.
 
 class Compute:
     """Understanding Private Variables '_' or '__'"""
-    def __init__(self):
-        self.__maxprice=900
+    def __init__(self,maxprice):
+        self.__maxprice=maxprice
 
-    #getter -If this is not defined __var will act as normal var
+    # #getter -If this is not defined __var will act as normal var
     def get_maxprice(self):
+        print('id under getter: ',id(self.__maxprice))
         print('Price:',self.__maxprice)
 
     #setter
@@ -18,17 +19,19 @@ class Compute:
         self.__maxprice=value
 
 
-c=Compute()
+c=Compute(500)
 c.get_maxprice()
-#value will not get updated
-c.__maxprice =100
+# actual value will not get updated and it will read it as different varaible
+c.__maxprice =1200
+print('id outside: ',id(c.__maxprice))
+print(c.__maxprice)
 c.get_maxprice()
 #calling setter to update value
-c.set_maxprice(1000)
+c.set_maxprice(2000)
 c.get_maxprice()
 
 #docstring and help
-print(Compute.__doc__)
-help(Compute)
+# print(Compute.__doc__)
+# help(Compute)
 
 
